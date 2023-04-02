6b515c78a3fbffe7cc5897d78471b8b1337535e1
Olga2213 <128790398+Olga2213@users.noreply.github.com>
2023-04-02 16:38:37 +0300

    Add files via upload

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Comment
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'comment']
+
+    def __init__(self, *args, **kwargs):
+        """Save the request with the form so it can be accessed in clean_*()"""
+        self.request = kwargs.pop('request', None)
+        super(CommentForm, self).__init__(*args, **kwargs)
